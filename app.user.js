04bae79c71736a6bef9514f30dfadc7de62cd9c2
supermomonga <hi@supermomonga.com>
2014-06-14 11:42:42 +0900

    hai

---
+++ app.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// == @author supermomonga
+// == @description Very benri.
+// == @copyright 2014, @supermomonga (http://blog.supermomonga.com)
+// == @icon http://www.gravatar.com/avatar/732238f1df7ff5ab6e8032878047364c.jpg?s=128
+// == @license The MIT License (MIT); http://opensource.org/licenses/MIT
+// ==/UserScript==
+
+(function(){
+  document.getElementById('question_submit_twitter').checked = true
+})();
